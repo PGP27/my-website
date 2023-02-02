7fe914169c871eb41783cc71345848b3e2c8f14a
pedro <peg.gep27@gmail.com>
2023-02-02 20:24:33 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -47,28 +47,38 @@ const App = () => {
   }, []);
 
   return (
-    <div className='relative h-screen w-screen overflow-hidden'>
-      <img
-        ref={rocketRef}
-        onLoad={onRocketLoad}
-        src={rocket}
-        alt=''
-        className='absolute transform rotate-45'
+    <div className='relative h-screen w-screen bg-[#28272F] text-white font-poppins overflow-hidden'>
+      <div
         style={{
-          width: `calc(${planetSize?.width}px / 16)`,
-          left: `calc(50% - ${rocketSize?.width}px / 2)`,
-          bottom: `calc(${planetSize?.height}px / 6)`,
+          backgroundImage:
+            'linear-gradient(rgba(40, 39, 47, 0.9), rgba(40, 39, 47, 0.9)), url(src/assets/space2.jpg)',
         }}
-      />
-      <img
-        id='planet'
-        ref={planetRef}
-        onLoad={onPlanetLoad}
-        src={planet}
-        alt=''
-        className='absolute w-full'
-        style={{ bottom: `calc(-${planetSize?.height}px / 1.5)` }}
-      />
+        className="h-full w-full bg-[url('src/assets/space.jpg')] bg-fixed bg-center bg-no-repeat bg-cover bg-opacity-20"
+      >
+        <h1 className='text-5xl'>Bem vindo, viajante!</h1>
+        <p>Sou o Pedro</p>
+        <img
+          ref={rocketRef}
+          onLoad={onRocketLoad}
+          src={rocket}
+          alt=''
+          className='absolute transform rotate-45'
+          style={{
+            width: `calc(${planetSize?.width}px / 16)`,
+            left: `calc(50% - ${rocketSize?.width}px / 2)`,
+            bottom: `calc(${planetSize?.height}px / 6)`,
+          }}
+        />
+        <img
+          id='planet'
+          ref={planetRef}
+          onLoad={onPlanetLoad}
+          src={planet}
+          alt=''
+          className='absolute w-full'
+          style={{ bottom: `calc(-${planetSize?.height}px / 1.5)` }}
+        />
+      </div>
     </div>
   );
 };
